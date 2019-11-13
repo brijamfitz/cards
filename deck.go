22b92e51636df4e8c13988b7c7aab30d71ebdb5d
brianjfitzgerald@gmail.com <bfitzg670@Brians-MacBook-Pro.local>
2019-11-13 12:00:18 -0500

    writefile func

--- deck.go
+++ deck.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // create a new type of 'deck'
 // which is a slice of strings
-
 type deck []string
 
 func newDeck() deck {
@@ -30,4 +33,14 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// need to convert deck into byte slice and save to hd
+// first turn the deck into a string
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
